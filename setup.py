3f5268eaf58382b7c6d9ba5f9b0b07dd4ac1d7ea
hcji <ji.hongchao@foxmail.com>
2019-03-07 11:01:21 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='rdchiral',
+      version='0.0.1',
+      description="Wrapper for RDKit's RunReactants to improve stereochemistry handling",
+      license='MIT',
+      author='connorcoley',
+      author_email='unknown',
+      url='https://github.com/connorcoley/rdchiral',
+      packages=find_packages()
+     )
